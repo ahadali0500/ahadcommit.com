3ceb49c42d9d2660af213cc308d10786f45c7758
Ahad Ali <ahad.ali@emergimentors.com.au>
2025-07-20 11:07:54 +0500

    Refactor layout to integrate Google Tag Manager scripts and remove commented-out JavaScript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,27 +22,31 @@ export default function RootLayout({
         <link rel="stylesheet" href="/assets/css/responsive.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
-
-        {/* <script src="/assets/js/jquery.min.js"></script> */}
-        {/* <script src="/assets/js/bootstrap.bundle.min.js"></script>
-          <script src="/assets/js/smooth-scroll.js"></script>
-          <script src="/assets/js/appear.min.js"></script>
-          <script src="/assets/js/wow.min.js"></script>
-          <script src="/assets/js/gsap.min.js"></script>
-          <script src="/assets/js/one-page-nav.js"></script>
-          <script src="/assets/js/lightcase.js"></script>
-          <script src="/assets/js/owl.carousel.min.js"></script>
-          <script src="/assets/js/swiper.min.js"></script>
-          <script src="/assets/js/imagesloaded-pkgd.js"></script>
-          <script src="/assets/js/isotope.pkgd.min.js"></script>
-          <script src="/assets/js/odometer.min.js"></script>
-          <script src="/assets/js/magnific-popup.js"></script>
-          <script src="/assets/js/main.js"></script>
-             <script src="/assets/js/main.js"></script>
-             <script src="/assets/js/jquery.min.js"></script> */}
-             
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TJXR9MXK');
+            `,
+          }}
+        />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TJXR9MXK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <HolyLoader
           color="linear-gradient(to right, #8e2de2, #8750f7)"
           height="3px"
@@ -50,7 +54,7 @@ export default function RootLayout({
           easing="linear"
         />
         {children}
-        
+
       </body>
     </html>
   );
